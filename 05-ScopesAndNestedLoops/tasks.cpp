69abfbb47a91aac7fe4f7dec6e3e8fd3afdd286a
Teodor Hristov <tedotedo771@gmail.com>
2020-11-12 17:02:16 +0200

    add task 7

--- 05-ScopesAndNestedLoops/tasks.cpp
+++ 05-ScopesAndNestedLoops/tasks.cpp
@@ -8,43 +8,48 @@ bool isDigitsSame(int number);
 int exponentialPower(int i);
 int sumTask3(int n);
 void printAllLinearIndependantVectors(unsigned int m, unsigned int n);
+void printAllNDigitPowers(unsigned int n);
 
 using std::cin;
 using std::cout;
 using std::endl;
 
 int main(){
-    //task 1
+    #pragma region task1
     // unsigned int n;
     // cin >> n;
 
     // cout << sum(n) << endl;
-
+    #pragma endregion task1
+    #pragma region task2
     //task 2
     // int a, b;
     // cin >> a >> b;
 
     // cout << sumOfAllPrimeNumbers(a,b) << endl;
-
+#pragma endregion task2
+    #pragma region task3
     //task 3
     //printNumbers();
-
+    #pragma endregion task3
+    #pragma region task4
     //task 4
     // int n = 2;
 
     // cout << sumTask3(n) << endl;
-
-    // return 0;
-
+    #pragma endregion task4
+    #pragma region task6
     //task 6
-    int m  = 1, n = 2;
-    printAllLinearIndependantVectors(m,n);
+    // int m  = 1, n = 2;
+    // printAllLinearIndependantVectors(m,n);
+#pragma endregion task6
+    #pragma region task7
+//task 7
+    int n = 7;
+    printAllNDigitPowers(n);
+    #pragma endregion task7
 }
-// 1 2
-// (1,1)
-// (1,2)
-// (2,1)
-// (2,2)
+
 void printAllLinearIndependantVectors(unsigned int m, unsigned int n){
     for(int i = m; i <= n; i++) {
         for(int j = m; j <= n; j++) {
@@ -62,11 +67,34 @@ void printAllLinearIndependantVectors(unsigned int m, unsigned int n){
 
         }
     }
+
 }
 
+void printAllNDigitPowers(unsigned int n) {
+    unsigned int lastNumber = 1;
+    unsigned int square = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        lastNumber *= 10;
+    }
+
+
+    for (int j = 2; j <= lastNumber; j++) {
+        square = j*j;
+
+        if(!(j & 1)) {
+            if(square <= lastNumber) {
+                cout << square << endl;
+            }
+        }
+    }
+    
+    
+}
 int sum (unsigned int n){
-    int sum = 0;
-    for (int i = 1; i <= n; i++)
+    unsigned int sum = 0;
+    for (unsigned int i = 1; i <= n; i++)
     {
         sum += i;
     }
