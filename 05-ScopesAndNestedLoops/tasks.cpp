9c7504d1e159b6ea5765bc0597c0d155775cbd5d
Teodor Hristov <tedotedo771@gmail.com>
2020-11-16 16:30:24 +0200

    add task 10 but it is not working

--- 05-ScopesAndNestedLoops/tasks.cpp
+++ 05-ScopesAndNestedLoops/tasks.cpp
@@ -9,6 +9,9 @@ int exponentialPower(int i);
 int sumTask3(int n);
 void printAllLinearIndependantVectors(unsigned int m, unsigned int n);
 void printAllNDigitPowers(unsigned int n);
+void findFirstNAutomorphNumbers(unsigned n);
+void encryptNumber(const int number);
+int mirrorNumber(int number);
 
 using std::cin;
 using std::cout;
@@ -44,12 +47,89 @@ int main(){
     // printAllLinearIndependantVectors(m,n);
 #pragma endregion task6
     #pragma region task7
-//task 7
-    int n = 7;
-    printAllNDigitPowers(n);
+
+    //task 7
+    // int n = 7;
+    // printAllNDigitPowers(n);
     #pragma endregion task7
+    #pragma region task8
+
+    // unsigned n;
+    // cin >> n;
+
+    // findFirstNAutomorphNumbers(n);
+
+    #pragma endregion task8
+    #pragma region task9
+
+    // unsigned n;
+
+    // cin >> n;
+
+    // encryptNumber(n);
+
+    #pragma endregion task9
+    #pragma region task10
+
+     unsigned n;
+
+     cin >> n;
+
+     decryptNumber(n);
+
+    #pragma endregion task10
+}   
+
+int mirrorNumber(const int number) {
+    int newNumber = 0;
+    int tempNumber = number;
+    while(tempNumber) {
+        newNumber += tempNumber % 10;
+        
+        if(tempNumber/10){
+            newNumber *= 10;
+        }
+
+        tempNumber /= 10;
+    }
+    return newNumber;
 }
 
+void encryptNumber(const int number) {
+    int tempNumber;
+
+    for( tempNumber = mirrorNumber(number) ; tempNumber; tempNumber /= 10) {
+        for(int i = tempNumber % 10; i > 0; i--) {
+            cout << 1;
+        }
+        cout << ((tempNumber / 10) ? "0" : "");;
+    }
+}
+
+void findFirstNAutomorphNumbers(unsigned n) {
+    unsigned temp = 0;
+
+    for(unsigned i = 5, count = 0; count < n ; i++) {
+        temp = i*i;
+
+        if(i > 0 && i < 9) {
+            if(temp % 10 == i) {
+                count++;
+                cout << i << endl;
+            }   
+        } else if(i > 9 && i < 100) {
+            if(temp % 100 == i) {
+                cout << i << endl;
+                count++;
+            }
+        } else {
+            if(temp % 1000 == i) {
+                cout << i << endl;
+                count++;
+            }
+        }
+    }
+}
 void printAllLinearIndependantVectors(unsigned int m, unsigned int n){
     for(int i = m; i <= n; i++) {
         for(int j = m; j <= n; j++) {
@@ -74,13 +154,13 @@ void printAllNDigitPowers(unsigned int n) {
     unsigned int lastNumber = 1;
     unsigned int square = 0;
 
-    for (int i = 0; i < n; i++)
+    for (unsigned int i = 0; i < n; i++)
     {
         lastNumber *= 10;
     }
 
 
-    for (int j = 2; j <= lastNumber; j++) {
+    for (unsigned int j = 2; j <= lastNumber; j++) {
         square = j*j;
 
         if(!(j & 1)) {
